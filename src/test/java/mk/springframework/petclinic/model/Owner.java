1793deb1b186b510ed8c255ff76495754fa79c98
Mustafa Kurt <info.mkurt@gmail.com>
2020-05-22 13:00:06 +0300

    [IMP] adding POJOs. Closes #2.

---
+++ src/test/java/mk/springframework/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package mk.springframework.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/test/java/mk/springframework/petclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package mk.springframework.petclinic.model;
+
+public class Vet extends Person {
+
+
+}
