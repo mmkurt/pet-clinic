20691b3bd6cef64bd13ac4bdf9ab281d585fa049
Mustafa Kurt <info.mkurt@gmail.com>
2020-05-24 23:25:29 +0300

    Added specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/mk/springframework/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package mk.springframework.petclinic.services;
+
+import mk.springframework.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends  CrudService<Speciality, Long> {
+}
